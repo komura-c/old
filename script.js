3ea729efebda1f608db2101a4b5597a6ba839cf2
komura <affirman03139@outlook.jp>
2020-04-02 19:31:26 +0900

    fix text

--- script.js
+++ script.js
@@ -62,16 +62,6 @@ function scrollScreenInt(starty, desty, newy, tick) {
 }
 
 /* スクロールフェイドアニメーション */
-var scrollAnimationElm = document.querySelectorAll('.sa');
-var scrollAnimationFunc = function () {
-  for (var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 300;
-    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-      scrollAnimationElm[i].classList.add('show');
-    }
-  }
-}
-
 var scrollAnimationElm = document.querySelectorAll('.fade');
 var scrollAnimationFunc = function () {
   for (var i = 0; i < scrollAnimationElm.length; i++) {
