1928c5fd3899bb21876c943a848f95abf96912f8
komura <affirman03139@outlook.jp>
2020-04-05 04:47:57 +0900

    update cdn&js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/* 画像遅延読み込み */
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+  img.setAttribute('src', img.getAttribute('data-src'));
+});
+
 /* スムーズスクロールアニメーション */
 function LinkClick(id) {
   var top = getElementAbsoluteTop(id);
@@ -43,10 +48,10 @@ function scrollScreenInt(starty, desty, newy, tick) {
 }
 
 /* スクロールフェイドアニメーション */
-var scrollAnimationElm = document.querySelectorAll('.fade');
-var scrollAnimationFunc = function () {
-  for (var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 300;
+const scrollAnimationElm = document.querySelectorAll('.fade');
+const scrollAnimationFunc = function () {
+  for (let i = 0; i < scrollAnimationElm.length; i++) {
+    const triggerMargin = 300;
     if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
       scrollAnimationElm[i].classList.add('show');
     }
@@ -56,9 +61,9 @@ window.addEventListener('load', scrollAnimationFunc);
 window.addEventListener('scroll', scrollAnimationFunc);
 
 /* サウンドクラウド遅延読み込み */
-var soundcloud__box = document.getElementById('soundcloud__box');
-var soundcloudLazyLoad = function () {
-  var addiframe = '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/524995878&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/524995476&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/436888101&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>';
+const soundcloud__box = document.getElementById('soundcloud__box');
+const soundcloudLazyLoad = function () {
+  const addiframe = '<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/524995878&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/524995476&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/436888101&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>';
   soundcloud__box.insertAdjacentHTML('beforebegin', addiframe);
 }
 window.addEventListener('load', soundcloudLazyLoad);
