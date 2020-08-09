7026e708eabd26352b90d43a96a8018c08965d8f
komura <affirman03139@outlook.jp>
2020-08-10 07:54:05 +0900

    feat(sound, fade): 音源を1つに、fadeアニメーションを削除

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-$(window).scroll(function () {
-  $(".js-fade").each(function () {
-    const position = $(this).offset().top,
-      scroll = $(window).scrollTop(),
-      windowHeight = $(window).height();
-    if (scroll > position - windowHeight + 100) {
-      $(this).addClass("js-show");
-    }
-  });
-});
-
 $(".js-nav-link").on("click", function () {
   const adjust = -20,
     speed = 400,
