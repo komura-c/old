970b7db3d8f06bf34e8528fe33bafe06185aa02c
komura <affirman03139@outlook.jp>
2020-08-12 13:53:35 +0900

    fix(js): soundcloudのiframeとjqueryを修正

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-const links = document.querySelectorAll(".js-nav-link");
-links.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    const id = link.getAttribute("href").replace("#", "");
-    const rectTop = document.getElementById(id).getBoundingClientRect().top,
-      position = window.pageYOffset,
-      buffer = 50,
-      top = rectTop + position - buffer;
-    window.scrollTo({
-      top,
-      behavior: "smooth",
-    });
-  });
+$(".js-nav-link").on("click", function () {
+  const adjust = -20,
+    speed = 400,
+    href = $(this).attr("href"),
+    target = $(href == "#" || href == "" ? "html" : href),
+    position = target.offset().top + adjust;
+  $("html").animate({ scrollTop: position }, speed, "swing");
+  return false;
 });
